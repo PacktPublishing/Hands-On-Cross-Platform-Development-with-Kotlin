d29a5ab8099e04ffa8cdca0ce6238be52406595d
Milos Vasic <milos85vasic@gmail.com>
2018-05-06 12:36:50 +0200

    Kotlin Native meets Android, first touches.

---
+++ Chapter03/src/main/kotlin/screen.kt
@@ -0,0 +1,9 @@
+import kotlinx.cinterop.*
+import platform.android.*
+import platform.posix.*
+import platform.gles3.*
+import platform.linux.*
+
+fun main(args: Array<String>) {
+    info("Welcome to Kotlin Native.")
+}
