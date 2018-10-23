f5272b3864c908cb3f2a4887f6680a9ff3ef4c28
Milos Vasic <milos85vasic@gmail.com>
2018-10-23 15:28:03 +0200

    Workin on Chapter 03.

--- Chapter03/src/main/kotlin/screen.kt
+++
@@ -1,9 +0,0 @@
-import kotlinx.cinterop.*
-import platform.android.*
-import platform.posix.*
-import platform.gles3.*
-import platform.linux.*
-
-fun main(args: Array<String>) {
-    info("Welcome to Kotlin Native.")
-}
