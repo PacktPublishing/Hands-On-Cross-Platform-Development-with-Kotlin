d6b3357d81adfbe9163a3ec170a5857715f93668
Milos Vasic <milos85vasic@gmail.com>
2018-10-24 12:33:02 +0200

    Hands on Kotlin Native - Chapter 03 - Immutability.

--- Chapter03/05_Immutability/src/main/kotlin/main.kt
+++ Chapter03/05_Immutability/src/main/kotlin/main.kt
@@ -1,15 +1,14 @@
 import konan.worker.InvalidMutabilityException
 
-enum class Dummies {
-    A, B, C;
-
-    var value = 0
+object X {
+    var y = 5
 }
 
 fun main(args: Array<String>) {
     try {
-        Dummies.A.value = 100
-    } catch (e: InvalidMutabilityException){
+        X.y = 10
+        print(X.y)
+    } catch (e: InvalidMutabilityException) {
         print(e)
     }
 }
