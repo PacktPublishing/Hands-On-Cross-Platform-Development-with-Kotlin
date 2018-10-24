f7154df0f36d3be7d7d9adb6a8458508c6e6f76c
Milos Vasic <milos85vasic@gmail.com>
2018-10-24 10:39:00 +0200

    Hands on Kotlin Native - Chapter 03 - Immutability.

--- Chapter03/02_Immutability/src/main/kotlin/main.kt
+++ Chapter03/02_Immutability/src/main/kotlin/main.kt
@@ -1,10 +1,30 @@
-import kotlinx.cinterop.IntVar
-import kotlinx.cinterop.nativeHeap
+import konan.worker.InvalidMutabilityException
+import konan.worker.freeze
+
+data class Student(
+        var firstName: String,
+        var lastName: String,
+        var email: String
+)
 
 fun main(args: Array<String>) {
-    val placement = nativeHeap.alloc(1, 0)
+    val student = Student(
+            "John",
+            "Smith",
+            "john@smith.com"
+    )
 
+    student.firstName = "New name"
+    student.lastName = "New last name"
+    student.email = "john2@example.com"
 
-    // Allocating some native memory:
-//    val myInteger = placement.alloce
+    student.freeze()
+
+    try {
+        student.firstName = "New name 2"
+        student.lastName = "New last name 2"
+        student.email = "john3@example.com"
+    } catch (e: InvalidMutabilityException) {
+        print(e)
+    }
 }
