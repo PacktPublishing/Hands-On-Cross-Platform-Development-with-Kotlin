0278db550812b32a6d03d5eed12d8ae5d642728b
Milos Vasic <milos85vasic@gmail.com>
2018-10-26 13:56:31 +0200

    Hands on Kotlin Native - Chapter 03 - Transferring objects.

---
+++ Chapter03/10_Transferring_Objects/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "10_Transferring_Objects"
---
+++ Chapter03/10_Transferring_Objects/src/main/kotlin/main.kt
@@ -0,0 +1,24 @@
+import konan.worker.TransferMode
+import konan.worker.startWorker
+
+data class Data(var value: Double)
+
+fun main(args: Array<String>) {
+
+    val worker = startWorker()
+    val mode = TransferMode.CHECKED
+
+    val data = Data(1000.0)
+
+    try {
+        worker.schedule(mode, { data }) { it ->
+            println("Item: ${it.value}")
+        }.result()
+    } catch (e: IllegalStateException){
+        println(e)
+    }
+
+    val processScheduledJobs = true
+    worker.requestTermination(processScheduledJobs)
+}
+
