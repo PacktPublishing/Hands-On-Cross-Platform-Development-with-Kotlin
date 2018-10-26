abca941a26aa9607afc70f5d28272182f12f6207
Milos Vasic <milos85vasic@gmail.com>
2018-10-26 12:29:39 +0200

    Hands on Kotlin Native - Chapter 03 - Workers.

---
+++ Chapter03/08_Workers/src/main/kotlin/main.kt
@@ -0,0 +1,23 @@
+import konan.worker.TransferMode
+import konan.worker.startWorker
+import platform.posix.pow
+
+data class Data(val value: Double)
+
+fun main(args: Array<String>) {
+
+    val worker = startWorker()
+    val mode = TransferMode.CHECKED
+
+    for (x in 0..10) {
+        val future = worker.schedule(mode, { Data(x.toDouble()) }) { it ->
+            println("Value: ${it.value}")
+            pow(it.value, 2.0)
+        }
+
+        val result = future.result()
+        println("Power: $result\n- - - - - - - - -")
+    }
+    val processScheduledJobs = true
+    worker.requestTermination(processScheduledJobs)
+}
