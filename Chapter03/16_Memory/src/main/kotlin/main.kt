5b1b43dd9adcfe285333e20cb74e0a40cea79563
Milos Vasic <milos85vasic@gmail.com>
2018-11-22 12:57:56 +0100

    Hands on Kotlin Native - Chapter 03 - Memory.

--- Chapter03/16_Memory/src/main/kotlin/main.kt
+++ Chapter03/16_Memory/src/main/kotlin/main.kt
@@ -1,6 +1,13 @@
 import kotlinx.cinterop.*
 
 fun main(args: Array<String>) {
+    // Allocate memory:
+    val allocated = nativeHeap.alloc<IntVar>()
 
+    // Set the value:
+    allocated.value = 100
+    println("Value is: ${allocated.value}")
 
+    // De allocate memory:
+    nativeHeap.free(allocated)
 }
