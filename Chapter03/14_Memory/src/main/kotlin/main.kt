3a801e8a2945413bb58944af03845a6e6a24a688
Milos Vasic <milos85vasic@gmail.com>
2018-11-22 10:32:54 +0100

    Hands on Kotlin Native - Chapter 03 - Memory.

--- Chapter03/14_Memory/src/main/kotlin/main.kt
+++ Chapter03/14_Memory/src/main/kotlin/main.kt
@@ -1,3 +1,18 @@
+import kotlinx.cinterop.*
+
 fun main(args: Array<String>) {
 
+    memScoped {
+        var number = 0
+        val length = 1024
+        val data = allocArray<IntVar>(length)
+
+        for (x in 0..length) {
+            data[x] = number++
+        }
+
+        for (x in 0..length) {
+            println("Value: ${data[x]}")
+        }
+    }
 }
